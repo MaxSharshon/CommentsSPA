c158ebbadf7bdde856aa05a11ea6d627e84ed0b2
MaxSharshon <sharshonmax@gmail.com>
2025-09-23 18:50:21 +0300

    Relocate files and add docker

--- backend/Comments.API/Program.cs
+++ backend/Comments.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<CommentsContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddAutoMapper(cfg =>
 {
@@ -31,7 +31,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<CommentsContext>();
+    db.Database.Migrate();
+}
+
+var useHttps = app.Configuration.GetValue<bool?>("UseHttpsRedirection") ?? app.Environment.IsDevelopment();
+if (useHttps)
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
